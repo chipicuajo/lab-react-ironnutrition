02d08ec878ef57539e877c20e6c446a14b99e7e0
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-05-05 17:12:50 +0200

    IT1 & 2 done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 import foods from './foods.json';
+import FoodBox from './components/FoodBox';
 
-function App() {
+class App extends Component {
+  render(){
+    
   return (
-    <div >
-      
+    <div>{
+        foods.map((e, i) => {
+          return <FoodBox key={i} name={e.name} calories={e.calories} image={e.image}/>
+        })
+        }
     </div>
   );
 }
-
+}
 export default App;
